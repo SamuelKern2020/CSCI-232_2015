cb9b4f784223bec2258d6df6598cff7ccd819741
Samuel Kern <kern08@live.com>
2015-04-08 13:09:28 -0600

    Finished Floyd's Algorithm

--- CSCI_232_Lab3_FloydsAlgorithm/src/lab3/FloydsDriver.java
+++ CSCI_232_Lab3_FloydsAlgorithm/src/lab3/FloydsDriver.java
@@ -1,30 +1,31 @@
 package lab3;
 
+import java.util.Scanner;
+
 public class FloydsDriver {
 
 	final static int MAX_VALUE = Integer.MAX_VALUE/2;
-	//GraphData graph1 = new GraphData();
+	
+	
+	public static char [] letters = {'A','B','C','D','E'};
+	public static int numPoints = 5;
+	public static int [][] matrix = new int [numPoints][numPoints];
+	
 	
 	
 	public static void main(String[] args) {
 		
-		//Notes for this program assignment:
-		/*
-		 * Create a 2-D array w/ hardcoded edges between vertices (this should maybe be in a seperate class - that way a user could modify the 2-D array in a seperate class
-		 * 
-		 * Execute Floyd's algorithm to go through the 2-D array and update edges if edge weight is less than the current weight
-		 * - Essentially a triple "for" loop
-		 * 
-		 * Print each step
-		 */
-		
-		
 		//Define the graph:
-		
-		char [] letters = {'A','B','C','D','E'};
-		int numPoints = 5;
-		int [][] matrix = new int [numPoints][numPoints];
-		
+		initialize();
+		//Print out the initial graph:
+		System.out.println("Adjacency Matrix:");
+		printGraph();
+		//Perform Floyd's algorithm to find shortest paths:
+		runFloyds();
+
+	}
+	
+	private static void initialize() {
 		for(int y=0; y<numPoints; y++){
 			for (int x=0; x<numPoints; x++){
 				if(x==y){
@@ -33,9 +34,9 @@ public class FloydsDriver {
 				else{
 					matrix[x][y] = MAX_VALUE;
 				}
-				
 			}
 		}
+		//Initialize distances:
 		matrix[1][0] = 50;
 		matrix[3][0] = 80;
 		matrix[2][1] = 60;
@@ -45,8 +46,33 @@ public class FloydsDriver {
 		matrix[4][3] = 70;
 		matrix[1][4] = 50;
 		
-		//printGraph(matrix, numPoints);
+	}
+	
+	private static void runFloyds() {
+		Scanner in = new Scanner(System.in);
+		// Compute successively better paths through vertex k.
+		for (int k=0; k<numPoints;k++) {
+
+	  		// Do so between each possible pair of points.
+	  		for (int i=0; i<numPoints; i++) {
+	    		for (int j=0; j<numPoints;j++) {
+	    
+	      			if (matrix[i][k]+matrix[k][j] < matrix[i][j]) {
+	      				matrix[i][j] = matrix[i][k]+matrix[k][j];
+	      				System.out.println("All-pairs shortest paths are:\n");
+	      				printGraph();
+	      				System.out.println("Press enter to continue:");
+	      				in.nextLine();
+      				}		
+      			}
+      		}
+  		}
+	}
+	
+
+	public static void printGraph() {	
 		System.out.println("   A    B    C    D    E");
+		System.out.println("   ======================");
 		for(int y=0; y<numPoints; y++){
 			System.out.print(letters[y] + "  ");
 			for (int x=0; x<numPoints; x++){
@@ -62,57 +88,5 @@ public class FloydsDriver {
 			}
 			System.out.println("\n");
 		}
-		
-	
-	// Compute successively better paths through vertex k.
-	for (int k=0; k<numPoints;k++) {
-
-  		// Do so between each possible pair of points.
-  		for (int i=0; i<numPoints; i++) {
-    		for (int j=0; j<numPoints;j++) {
-    		
-    
-      			if (matrix[i][k]+matrix[k][j] < matrix[i][j]) {
-      				matrix[i][j] = matrix[i][k]+matrix[k][j];
-      				System.out.println("   A    B    C    D    E");
-      				for(int y=0; y<numPoints; y++){
-      					System.out.print(letters[y] + "  ");
-      					for (int x=0; x<numPoints; x++){
-      						if(matrix[x][y] == 0){
-      							System.out.print("__   ");
-      						}
-      						else if (matrix[x][y]==MAX_VALUE){
-      							System.out.print("INF  ");
-      						}
-      						else{
-      							System.out.print(matrix[x][y] + "   ");
-      						}
-      					}
-      					System.out.println("\n");
-      				}
-      				System.out.println();
-      				//path[i][j] = path[k][j];
-      			}
-      		}
-  		}
 	}
-	
-	
-//	public static void printGraph(int[][] graph, int n) {	
-//		System.out.println("A   B   C   D   E");
-//		for(int y=0; y<n; y++){
-//			for (int x=0; x<n; x++){
-//				if(graph[x][y]==0){
-//					System.out.print("__  ");
-//				}
-//				else{
-//					System.out.print(graph[x][y] + "  ");
-//				}
-//			}
-//			System.out.println("\n");
-//		}
-//	}
-	}
-
-	
 }
